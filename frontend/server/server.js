d8d6f5a1f2cf1767787161ab8806362b402a8345
shanuchauhan1211 <shanu1211chauhan@gmail.com>
2024-06-02 02:53:31 +0530

    cors

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -10,10 +10,13 @@ import Post from "./router/createPost.js";
 
 
 const app = express();
-
+const corsOptions = {
+    origin: 'artvista-lovat.vercel.app', // or use an array for multiple origins
+    optionsSuccessStatus: 200 // For legacy browser support
+  };
 
 app.use(express.json({limit:'50mb'}));
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use("/User",User);
 app.use("/Post",Post);
